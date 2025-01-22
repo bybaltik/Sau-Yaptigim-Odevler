d47bc099a0409436c51ddf43a25cdde68380fdbe
bedirhan baltık <133059418+bybaltik@users.noreply.github.com>
2025-01-22 21:20:49 +0300

    gf

--- WEB_PROGRAMLAMA_PROJESI_2024-main/WEB_PROGRAMLAMA_PROJESI_2024-main/WEB_PROGRAMLAMA_PROJESI_2024/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WEB_PROGRAMLAMA_PROJESI_2024.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
